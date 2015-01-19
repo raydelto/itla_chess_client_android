8a27dd023601be149a34dd637ce7eb048ec3ef6c
Christian D. Rodriguez <christian.softwaredeveloper@gmail.com>
2015-01-18 23:24:52 -0400

    Esto es una prueba
    	Estoy gestionando esta prueba para ver como hace el
    	procedimiento de push hacia un repositorio fork...

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba{
 
     public static void main(String[] args){
-        System.out.println("Prueba");
+        System.out.println("Aprendiendo a usar git para trabajar en ITLA Chess :D");
     }
 
 }
