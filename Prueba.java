ba197ac577aa4e2d7f567ebdadf830f34800c247
Christianrd <christian.softwaredeveloper@gmail.com>
2015-01-23 14:00:56 -0400

    Update Prueba.java

--- Prueba.java
+++ Prueba.java
@@ -2,7 +2,7 @@ public class Prueba{
 
     public static void main(String[] args){
         System.out.println("\tAprendiendo a usar git para trabajar en ITLA Chess. :D\n");
-        System.out.println("\n\t\tModified by Christian D. Rodriguez.")
+        System.out.println("\n\t\tModified by Christian D. Rodriguez.");
     }
 
 }
